b649fee991e24622c1e93d6d07199a4a48a90eb6
Pasarica Diana <pasaricadiana@yahoo.com>
2022-02-21 22:59:35 +0200

    added project files

---
+++ E-LearningApp_WEBAPI/ApplicationDbContext.cs
@@ -0,0 +1,6 @@
+﻿namespace E_LearningApp_WEBAPI
+{
+    internal class ApplicationDbContext
+    {
+    }
+}
---
+++ E-LearningApp_WEBAPI/Startup.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using DataLibrary.Models;
+using DataLibrary.DataContext;
+namespace E_LearningApp_WEBAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddDbContext<ELearningContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("AppConnectionString")));
+            services.AddDefaultIdentity<User>()
+              .AddEntityFrameworkStores<ELearningContext>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "E_LearningApp_WEBAPI", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "E_LearningApp_WEBAPI v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+           
+            app.UseAuthorization();
+            app.UseAuthentication();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
